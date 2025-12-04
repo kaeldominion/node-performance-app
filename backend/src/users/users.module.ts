97fcb8e32bfcff02509470fd1a1e81980e6a9f74
Kael Dominion <spencertarring@Spencers-MacBook-Pro.local>
2025-12-05 02:11:37 +0800

    Fix circular dependency between AuthModule and UsersModule using forwardRef

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController, AdminUsersController, SetupController } from './users.controller';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [forwardRef(() => AuthModule)],
   controllers: [UsersController, AdminUsersController, SetupController],
   providers: [UsersService],
   exports: [UsersService],
